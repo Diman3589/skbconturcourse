9e895924f4471112a5c477f06084f3c22f2a6a83
DmitryZ <dmitriyzagulyaev@gmail.com>
2016-09-29 03:44:24 +0500

    Added tests and code

---
+++ Triangle/Triangle.Tests/TriangleTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Program;
+
+namespace TriangleTests
+{
+    [TestClass]
+    public class TriangleTest
+    {
+
+
+        [TestMethod]
+        public void CheckTheAreaOfTriangleWithThreeSidesEqualToSix()
+        {
+            ThreeSides obj = new ThreeSides(3, 4, 5);
+            Triangle triangle = new Triangle(obj);
+
+            var result = triangle.CalculateArea();
+            result = Math.Round(result, 2);
+
+            Assert.AreEqual(result, 6);
+        }
+
+        [TestMethod]
+        public void CheckTheAreaOfTriangleWithTwoSidesAndOneAngleEqualToTwentyFour()
+        {
+            TwoSidesAndAngle obj = new TwoSidesAndAngle(6, 8, 90);
+            Triangle triangle = new Triangle(obj);
+
+            var result = triangle.CalculateArea();
+            result = Math.Round(result, 2);
+
+            Assert.AreEqual(result, 24);
+        }
+
+        [TestMethod]
+        public void CheckTheAreaOfTriangleWithOneSideAndTwoAnglesEqualToTwentyOne()
+        {
+            OneSideAndTwoAngles obj = new OneSideAndTwoAngles(10, 60, 30);
+            Triangle triangle = new Triangle(obj);
+
+            var result = triangle.CalculateArea();
+            result = Math.Round(result, 2);
+
+            Assert.AreEqual(result, 21.65);
+        }
+            
+    }
+}
--- Triangle/Triangle/Program.cs
+++ Triangle/Triangle/Program.cs
@@ -1,13 +1,145 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Triangle
+namespace Program
 {
+    public struct ThreeSides
+    {
+        public double SideA;
+        public double SideB;
+        public double SideC;
+
+        public ThreeSides(double a, double b, double c)
+        {
+            SideA = a;
+            SideB = b;
+            SideC = c;
+        }
+    }
+
+    public struct TwoSidesAndAngle
+    {
+        public double SideA;
+        public double SideB;
+        public double Angle;
+
+        public TwoSidesAndAngle(double a, double b, double angle)
+        {
+            SideA = a;
+            SideB = b;
+            Angle = angle;
+        }
+    }
+
+    public struct OneSideAndTwoAngles
+    {
+        public double Side;
+        public double AngleOne;
+        public double AngleTwo;
+
+        public OneSideAndTwoAngles(double a, double angleOne, double angleTwo)
+        {
+            Side = a;
+            AngleOne = angleOne;
+            AngleTwo = angleTwo;
+        }
+    }
+
     public class Triangle
     {
+        private readonly double _sideA;
+        private readonly double _sideB;
+        private readonly double _sideC;
+        private readonly double _angleOne;
+        private readonly double _angleTwo;
+        private double _angleThree;
+
+        public Triangle(ThreeSides obj)
+        {
+            _sideA = obj.SideA;
+            _sideB = obj.SideB;
+            _sideC = obj.SideC;
+            if (!CheckSide(ref _sideA) || !CheckSide(ref _sideB) || !CheckSide(ref _sideC))
+            {
+                throw new ArgumentException("Not valid side!");
+            }
+        }
+
+        public Triangle(TwoSidesAndAngle obj)
+        {
+            _sideA = obj.SideA;
+            _sideB = obj.SideB;
+            if (!CheckSide(ref _sideA) || !CheckSide(ref _sideB))
+            {
+                throw new ArgumentException("Not valid side!");
+            }
+
+            _angleOne = obj.Angle;
+            if (CheckAngle(new[] {_angleOne}))
+            {
+                _sideC = Math.Sqrt(Math.Pow(_sideA, 2) + Math.Pow(_sideB, 2) - 2*_sideA*_sideB*Math.Cos(ToRadian(_angleOne)));
+            }
+            else
+            {
+                throw new ArgumentException("Not valid angle!");
+            }
+        }
+
+        public Triangle(OneSideAndTwoAngles obj)
+        {
+            _sideC = obj.Side;
+            if (!CheckSide(ref _sideC))
+            {
+                throw new ArgumentException("Not valid side!");
+            }
+
+            _angleOne = obj.AngleOne;
+            _angleTwo = obj.AngleTwo;
+            if (CheckAngle(new[] {_angleOne, _angleTwo}))
+            {
+                _angleThree = 180 - (_angleOne + _angleTwo);
+                _sideA = _sideC*Math.Sin(ToRadian(_angleOne))/Math.Sin(ToRadian(_angleThree));
+                _sideB = _sideC*Math.Sin(ToRadian(_angleTwo))/Math.Sin(ToRadian(_angleThree));
+            }
+            else
+            {
+                throw new ArgumentException("Not valid angle!");
+            }
+        }
+
+        private bool CheckAngle(double[] angles)
+        {
+            var sumAngles = 0.0;
+
+            foreach (var angle in angles)
+            {
+                if (angle > 0 && angle < 180) sumAngles += angle;
+                else return false;
+            }
+            return sumAngles < 180;
+        }
+
+        public bool CheckSide(ref double side)
+        {
+            return side > 0;
+        }
+
+        private double ToRadian(double degrees)
+        {
+            return degrees*Math.PI/180;
+        }
+
+        public double CalculateArea()
+        {
+            var p = (_sideA + _sideB + _sideC)/2;
+            var area = Math.Sqrt(p*(p - _sideA)*(p - _sideB)*(p - _sideC));
+            return area;
+        }
+
         static void Main(string[] args)
         {
         }
