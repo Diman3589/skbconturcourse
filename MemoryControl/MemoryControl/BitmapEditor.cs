2ebfaf683947d7f136302a3fd65f07408381ac0d
DmitryZ <dmitriyzagulyaev@gmail.com>
2016-11-11 12:54:46 +0500

    Optimize SetPixel

--- MemoryControl/MemoryControl/BitmapEditor.cs
+++ MemoryControl/MemoryControl/BitmapEditor.cs
@@ -8,42 +8,40 @@ namespace MemoryControl
     class BitmapEditor : IDisposable
     {
         private readonly Bitmap _bitmap;
-        private Rectangle _rect;
-        private BitmapData _bmpData;
+        private readonly BitmapData _bmpData;
+        private readonly byte[] _rgbValues;
+        private readonly int _size;
 
         public BitmapEditor(Bitmap bitmap)
         {
             _bitmap = bitmap;
+            var rect = new Rectangle(0, 0, _bitmap.Width, _bitmap.Height);
+            _bmpData = _bitmap.LockBits(rect, ImageLockMode.ReadWrite, _bitmap.PixelFormat);
+            _size = _bmpData.Stride*_bmpData.Height;
+            _rgbValues = new byte[_size];
+            Marshal.Copy(_bmpData.Scan0, _rgbValues, 0, _size);
         }
+
 
         public void SetPixel(int x, int y, byte r, byte g, byte b)
         {
-            _rect = new Rectangle(x, y, _bitmap.Width - x, _bitmap.Height - y);
-            _bmpData = _bitmap.LockBits(_rect, ImageLockMode.ReadWrite, _bitmap.PixelFormat);
-
-            var ptr = _bmpData.Scan0;
-
-            const int bytes = 4;
-            var rgbValues = new byte[bytes];
-
-            Marshal.Copy(ptr, rgbValues, 0, bytes);
-
-            for (var i = 2; i < rgbValues.Length; i += 3)
+            if (x >= _bmpData.Width || y >= _bmpData.Height)
             {
-                rgbValues[i] = r;
-                rgbValues[i - 1] = g;
-                rgbValues[i - 2] = b;
+                throw new ArgumentException("not valid coordinates");
             }
 
-            Marshal.Copy(rgbValues, 0, ptr, bytes);
+            var pixelCoord = y*_bmpData.Stride + x*3;
 
-            _bitmap.UnlockBits(_bmpData);
-            _bitmap.Save("d:/tmp.bmp");
-
+            _rgbValues[pixelCoord] = r;
+            _rgbValues[pixelCoord + 1] = g;
+            _rgbValues[pixelCoord + 2] = b;
         }
 
         public void Dispose()
         {
+            Marshal.Copy(_rgbValues, 0, _bmpData.Scan0, _size);
+            _bitmap.UnlockBits(_bmpData);
+            _bitmap.Save("d:/tmp.bmp");
             _bitmap.Dispose();
         }
 
@@ -52,10 +50,40 @@ namespace MemoryControl
             var bitmap = (Bitmap) Image.FromFile("d:/sis.bmp");
             using (var bitmapEditor = new BitmapEditor(bitmap))
             {
-                //bitmapEditor.SetPixel(20, 10, 255, 0, 0);
-                //bitmapEditor.SetPixel(30, 10, 255, 0, 255);
-                bitmapEditor.SetPixel(40, 10, 255, 0, 0);
-                bitmapEditor.SetPixel(50, 10, 0, 255, 0);
+                var timer = new Timer();
+                using (timer.Start())
+                {
+                    for (var i = 0; i < 206; i++)
+                    {
+                        for (var j = 0; j < 137; j++)
+                        {
+                            var r = (byte) new Random().Next(0, 85);
+                            var g = (byte) new Random().Next(85, 170);
+                            var b = (byte) new Random().Next(170, 255);
+                            bitmapEditor.SetPixel(i, j, r, g, b);
+                        }
+                    }
+                }
+                Console.WriteLine((double)timer.ElapsedMilliseconds / 1000);
+
+                var timer1 = new Timer();
+            var bitmap1 = (Bitmap) Image.FromFile("d:/sis.bmp");
+                using (timer1.Start())
+                {
+                    for (var i = 0; i < 206; i++)
+                    {
+                        for (var j = 0; j < 137; j++)
+                        {
+                            var r = (byte) new Random().Next(85, 170);
+                            var g = (byte) new Random().Next(0, 170);
+                            var b = (byte) new Random().Next(170, 255);
+                            var color = Color.FromArgb(r,g,b);
+                            bitmap1.SetPixel(i, j, color);
+                        }
+                    }
+                }
+                Console.WriteLine((double)timer1.ElapsedMilliseconds / 1000);
+                Console.ReadLine();
             }
         }
     }
