ea9122c727e2e6dd11e8bfa351e75278831cd7ea
DmitryZ <dmitriyzagulyaev@gmail.com>
2016-11-12 20:30:57 +0500

    Change method SetPixel. Now whole image is locked and each pixel copied into bitmap

--- MemoryControl/MemoryControl/BitmapEditor.cs
+++ MemoryControl/MemoryControl/BitmapEditor.cs
@@ -9,37 +9,40 @@ namespace MemoryControl
     {
         private readonly Bitmap _bitmap;
         private readonly BitmapData _bmpData;
-        private readonly byte[] _rgbValues;
-        private readonly int _size;
 
         public BitmapEditor(Bitmap bitmap)
         {
             _bitmap = bitmap;
             var rect = new Rectangle(0, 0, _bitmap.Width, _bitmap.Height);
             _bmpData = _bitmap.LockBits(rect, ImageLockMode.ReadWrite, _bitmap.PixelFormat);
-            _size = _bmpData.Stride*_bmpData.Height;
-            _rgbValues = new byte[_size];
-            Marshal.Copy(_bmpData.Scan0, _rgbValues, 0, _size);
         }
 
+        private bool CheckCoordinates(int x, int y)
+        {
+            return x >= _bmpData.Width || y >= _bmpData.Height;
+        }
 
         public void SetPixel(int x, int y, byte r, byte g, byte b)
         {
-            if (x >= _bmpData.Width || y >= _bmpData.Height)
+            if (CheckCoordinates(x, y))
             {
                 throw new ArgumentException("not valid coordinates");
             }
 
-            var pixelCoord = y*_bmpData.Stride + x*3;
+            var scan = _bmpData.Scan0 + y*_bmpData.Stride + x*3;
+            var rgbValues = new byte[3];
 
-            _rgbValues[pixelCoord] = r;
-            _rgbValues[pixelCoord + 1] = g;
-            _rgbValues[pixelCoord + 2] = b;
+            Marshal.Copy(scan, rgbValues, 0, 3);
+
+            rgbValues[0] = r;
+            rgbValues[1] = g;
+            rgbValues[2] = b;
+
+            Marshal.Copy(rgbValues, 0, scan, 3);
         }
 
         public void Dispose()
         {
-            Marshal.Copy(_rgbValues, 0, _bmpData.Scan0, _size);
             _bitmap.UnlockBits(_bmpData);
             _bitmap.Save("d:/tmp.bmp");
             _bitmap.Dispose();
@@ -64,10 +67,10 @@ namespace MemoryControl
                         }
                     }
                 }
-                Console.WriteLine((double)timer.ElapsedMilliseconds / 1000);
+                Console.WriteLine((double) timer.ElapsedMilliseconds/1000);
 
                 var timer1 = new Timer();
-            var bitmap1 = (Bitmap) Image.FromFile("d:/sis.bmp");
+                var bitmap1 = (Bitmap) Image.FromFile("d:/sis.bmp");
                 using (timer1.Start())
                 {
                     for (var i = 0; i < 206; i++)
@@ -77,12 +80,12 @@ namespace MemoryControl
                             var r = (byte) new Random().Next(85, 170);
                             var g = (byte) new Random().Next(0, 170);
                             var b = (byte) new Random().Next(170, 255);
-                            var color = Color.FromArgb(r,g,b);
+                            var color = Color.FromArgb(r, g, b);
                             bitmap1.SetPixel(i, j, color);
                         }
                     }
                 }
-                Console.WriteLine((double)timer1.ElapsedMilliseconds / 1000);
+                Console.WriteLine((double) timer1.ElapsedMilliseconds/1000);
                 Console.ReadLine();
             }
         }
